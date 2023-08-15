0ca9bc89df4da684b6d6dcb3d97609ffa0b4dcf7
omarjafor <omarjaforchy@gmail.com>
2023-08-15 13:04:11 +0600

    bank deposit

--- js/deposit.js
+++ js/deposit.js
@@ -1,9 +1,15 @@
-document.getElementById('btn-deposit').addEventListener('click', function(){
+document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-field');
     const depositAmount = parseFloat(depositField.value);
 
-    const depositOld = document.getElementById('depositTotal');
-    const depositTotal = parseFloat(depositOld.innerText);
-    depositOld.innerText = depositAmount + depositTotal;
+    if (depositAmount >= 500) {
+        const depositOld = document.getElementById('depositTotal');
+        const depositTotal = parseFloat(depositOld.innerText);
+        depositOld.innerText = depositAmount + depositTotal;
+
+        const totalBalance = document.getElementById('totalBalance');
+        const totalAmount = parseFloat(totalBalance.innerText);
+        totalBalance.innerText = depositAmount + totalAmount;
+    }
     depositField.value = '';
 })
