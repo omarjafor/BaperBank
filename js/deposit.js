d8430271a151ebfdd55dd039e5fce0370471d31b
omarjafor <omarjaforchy@gmail.com>
2023-08-15 13:35:45 +0600

    bank withdraw

--- js/deposit.js
+++ js/deposit.js
@@ -14,8 +14,21 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     depositField.value = '';
 })
 
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const withdrawAmount = parseFloat(withdrawField.value);
-    
+
+    const totalBalance = document.getElementById('totalBalance');
+    const totalAmount = parseFloat(totalBalance.innerText);
+
+    if (withdrawAmount < totalAmount) {
+        const withdrawOld = document.getElementById('withdrawTotal');
+        const withdrawTotal = parseFloat(withdrawOld.innerText);
+        withdrawOld.innerText = withdrawAmount + withdrawTotal;
+
+        totalBalance.innerText = totalAmount - withdrawAmount;
+    } else {
+        alert('Not enough money! Check Your Balance!')
+    }
+    withdrawField.value = '';
 })
